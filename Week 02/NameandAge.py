5d3d1dd314b863de2b1c1f9b968a3aec9fcdb08c
Rebecca Feeley <G00425652@atu.ie>
2023-02-07 02:06:39 +0000

    Week 02 lab extra tasks

---
+++ Week 02/NameandAge.py
@@ -0,0 +1,11 @@
+# NameandAge.py
+# This program prints Hello Rebecca, your age is 21.
+# Author: Rebecca Feeley
+
+Name = input ("Enter your name: ")
+Age = input ("Enter your age: ")
+print ("Hello " + Name + " your age is " + Age)
+
+Name = input ("Enter your name: ")
+Age = input ("Enter your age: ")
+print ("Hello " + Name + "\tyour age is " + Age)
---
+++ Week 02/subtract.py
@@ -0,0 +1,13 @@
+# Subtract.py
+# Two ways of programming to give the result of subtracting two numbers
+# Author: Rebecca Feeley
+
+n1 = int("21")
+n2 = int("4")
+res = n1 - n2
+print ("The result is", res)
+
+number = 21 - 4
+print ("The result is", number)
+
+
