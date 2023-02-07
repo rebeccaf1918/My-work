56b310d90cb733af24fbee09ca33e488c272aa8b
Rebecca Feeley <G00425652@atu.ie>
2023-02-07 02:16:53 +0000

    Week02 lab extras

--- Week 02/subtract.py
+++ Week 02/subtract.py
@@ -1,5 +1,6 @@
 # Subtract.py
 # Two ways of programming to give the result of subtracting two numbers
+# Also determines if two numbers are equal
 # Author: Rebecca Feeley
 
 n1 = int("21")
@@ -11,3 +12,9 @@ number = 21 - 4
 print ("The result is", number)
 
 
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+if a == b:
+ print ("Both inputs are equal")
+else:
+ print ("Your inputs are not equal")
