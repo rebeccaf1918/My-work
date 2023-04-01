63c969f3acc39b055799d000dae02bbb0c9ec96e
Rebecca Feeley <G00425652@atu.ie>
2023-04-01 04:00:43 +0100

    weekly task updates

--- Week 08/plottask.py
+++ Week 08/plottask.py
@@ -1,7 +1,7 @@
 # Weekly Task 08
 # plottask.py
-#a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
-#and a plot of the function  h(x)=x3 in the range [0, 10], on the one set of axes.
+# Histogram of a normal distribution of 1000 values with a mean of 5 and standard deviation of 2, 
+# And a plot of the function h(x)=x3 in the range [0, 10], on the one set of axes.
 # Author: Rebecca Feeley
 
 import numpy as np 
@@ -12,10 +12,15 @@ np.random.seed(1)
 histogram = np.random.normal(loc=5, scale = 2, size = 1000)
 
 plt.hist(histogram)
-#plt.show()
+# plt.show()
 
 xpoints = np.array(range(1,11))
 ypoints = pow(xpoints, 3)
 
-plt.plot (xpoints, ypoints)
+plt.plot (xpoints, ypoints, color = 'g', label = "h(x)=x^3")
+
+plt.grid(linestyle = '--', linewidth = 1)
+plt.title("Weekly Task Plot")
+plt.legend()
+
 plt.show()
