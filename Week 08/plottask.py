e25bb0b0d634150a543b1afc984528475df4d6e1
Rebecca Feeley <G00425652@atu.ie>
2023-04-01 03:43:41 +0100

    mywork

--- Week 08/plottask.py
+++ Week 08/plottask.py
@@ -1,7 +1,7 @@
 # Weekly Task 08
 # plottask.py
-##a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
-##and a plot of the function  h(x)=x3 in the range [0, 10], on the one set of axes.
+#a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
+#and a plot of the function  h(x)=x3 in the range [0, 10], on the one set of axes.
 # Author: Rebecca Feeley
 
 import numpy as np 
